b7eebc5f6cde857a58e4b38ba7b81582b95c6d64
kazuhiro homma <kazu.homma@gmail.com>
2012-09-26 06:49:07 -0400

    add kazuph script

---
+++ scripts/yapc2012/kazuph.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# coding : utf-8
+t = Time.now
+max = 10000
+def prime?(num, ary)
+  ary.each do |n|
+    break if n * n  > num
+    return false if num % n == 0
+  end
+  return true
+end
+n = 0
+prime_ary = [2, 3, 5]
+while  prime_ary.count < max
+  n += 1
+  prime = 6 * n + 1
+  if prime?(prime, prime_ary)
+    prime_ary.push(prime)
+  end
+  prime += 4
+  if prime?(prime, prime_ary)
+    prime_ary.push(prime)
+  end
+end
+p prime_ary.inject(:+)
+p Time.now - t
