f96b07ec66ccba62abf171c3b9294fec556940d2
hidehigo <hidehigo@gmail.com>
2012-09-28 08:28:01 +0900

    rm dakogai2

--- scripts/yapc2012/dankogai2.pl
+++
@@ -1,2 +0,0 @@
-length q cmp lc and print chr oct oct ord qw q do q and print chr oct oct ord q mkdir m and print chr oct oct ord qw q for q and print chr oct oct oct ord q eq le and print chr oct ord uc qw q bind q and print chr oct ord uc q each ne and print chr oct oct ord qw q dump q and print chr oct oct oct ord q eq ne and print chr oct oct oct ord q eq ne
-
