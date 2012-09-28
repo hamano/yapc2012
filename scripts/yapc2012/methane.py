a05a73755d0ef1ab2ee0044421459297a0ab29f4
kazuhiro homma <kazu.homma@gmail.com>
2012-09-28 19:17:21 -0400

    modified methane.py

--- scripts/yapc2012/methane.py
+++ scripts/yapc2012/methane.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python -S
-MAX_NUM = 105000
+# coding: utf-8
+from math import log
 
-def main():
+def main(N):
+    if N <= 3:
+        return sum([2,3,5][:N])
+
+    # http://ja.wikipedia.org/wiki/素数定理
+    MAX_NUM = int(N*log(N) + N*log(log(N)) + 1)
+
     L = bytearray(MAX_NUM)
     F = b'1' * MAX_NUM
     S = 2
@@ -10,7 +17,7 @@ def main():
     L[::2] = F[::2]
 
     for i in xrange(3, MAX_NUM, 2):
-        if l >= 10000:
+        if l >= N:
             break
         if L[i]:
             continue
@@ -22,8 +29,8 @@ def main():
 
 #import sys
 #if 'bench' in sys.argv:
-#    assert 496165411 == main()
+#    assert 496165411 == main(10000)
 #    import timeit
-#    print timeit.timeit(main, number=100)
+#    print timeit.timeit(lambda: main(10000), number=100)
 
-print main()
+print main(10000)
