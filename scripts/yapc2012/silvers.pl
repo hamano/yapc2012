5976f09a4c74f3a59d4329f67323f548458d2e3d
kazuhiro homma <kazu.homma@gmail.com>
2012-09-29 05:39:15 -0400

    modifired silvers.js

--- scripts/yapc2012/silvers.pl
+++ scripts/yapc2012/silvers.pl
@@ -2,27 +2,27 @@
 use strict;
 use warnings;
 
-my $MAX_NUM = 104729; # 裏技
-my @table = (0..$MAX_NUM);
-my $result = 2 + 3; # 最初だけ特別扱い
+sub sum_primes {
+    my $MAX_PRIME_NUM = 104729; # 裏技
+    my $sum_of_primes = 2 + 3;  # 最初だけ特別扱い
+    my @table = ();
+    my $n = 0;
+    while ( 1 ) {
+        # 5以上の素数は6n-1, 6n+1であらわすことができる
+        $n += 6;
+        for my $num ($n - 1, $n + 1) {
+            next if $table[$num];
+            return $sum_of_primes if $num > $MAX_PRIME_NUM;
+            $sum_of_primes += $num;
 
-sub is_prime {
-    my $num = shift;
-    return unless $table[$num];
-    $result += $num;
-
-    # 素数の倍数はもういらないので判定ついでに落とす
-    my $multi = $num * $num;
-    while ( $multi <= $MAX_NUM ) {
-        delete $table[$multi];
-        $multi += $num;
+            # 素数の倍数はもういらないので判定ついでに落とす
+            my $multi = $num * $num;
+            while ( $multi <= $MAX_PRIME_NUM ) {
+                $table[$multi]++;
+                $multi += $num;
+            }
+        }
     }
 }
 
-# 5以上の素数は6n-1, 6n+1であらわすことができる
-my $n = 0;
-while ( $n < $MAX_NUM ) {
-    $n += 6;
-    is_prime($n + $_) for (-1, 1);
-}
-print $result;
+print sum_primes();
