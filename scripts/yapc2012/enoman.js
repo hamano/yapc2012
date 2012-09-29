e54051504ca2de4cc0ec582c1dfe77cad2702fb1
hidehigo <hidehigo@gmail.com>
2012-09-29 23:06:43 +0900

    update enoman.js

--- scripts/yapc2012/enoman.js
+++ scripts/yapc2012/enoman.js
@@ -1,22 +1,31 @@
-function primep(num){
-  if (num > 1) {
-    var i=2,j=Math.sqrt(num)
-    for (i;i<=j;i++){
-      if (num % i == 0){
+function isPrime(num){
+  if(num>=3){
+    if(num%2==0) {return false}
+    for (var i=3;i*i<=num;i+=2) {
+      if (num%i==0) {
         return false}}
     return true}
-  else{
-    return false}}
+  else if(num<2){return false}
+  else if(num==2){return true}
+  else {return false}}
 
-function next_prime(num){
-  for (num++;!primep(num);num++){}
+function nextOdd(num){
+  if(num%2==0){num++}
+  else{num+=2}
   return num}
 
-function sum_primes(num){
+function nextPrime(num){
+  if(num<2){return 2}
+  num=nextOdd(num)
+  for (num;!isPrime(num);num+=2){}
+  return num}
+
+function sumPrimes(num){
   var n=0,sum=0,nth_prime=0
   for (n;n<num;n++){
-    nth_prime = next_prime(nth_prime)
+    nth_prime = nextPrime(nth_prime)
     sum += nth_prime}
   return sum}
 
-console.log(sum_primes(10000))
+console.log(sumPrimes(10000))
+
