79858fa975e032464109a48359e25a252691efbc
kazuhiro homma <kazu.homma@gmail.com>
2012-09-29 05:38:03 -0400

    modified watilde.js

--- scripts/yapc2012/watilde.js
+++ scripts/yapc2012/watilde.js
@@ -1,31 +1,27 @@
-function isPrime(n) {
-  if (n < 2) {
-    return 0;
-  } else if (n === 2) {
+var Prime = {
+  is: function (n) {
+    "use strict";
+    if (n < 2) { return 0; }
+    if (n === 2) { return 1; }
+    if (n % 2 === 0) { return 0; }
+    var i = 3;
+    for (i; i * i <= n; i += 2) {
+      if (n % i === 0) { return 0; }
+    }
     return 1;
-  }
-  if (n % 2 === 0) {
-    return 0;
-  }
-  var i = 3;
-  for (i; i * i <= n; i += 2) {
-    if (n % i === 0) {
-      return 0;
+  },
+  calc: function (stdin) {
+    "use strict";
+    var i = 1, count = 1, stdout = 0;
+    for (i; stdin >= count; i += 1) {
+      if (this.is(i) === 1) {
+        count += 1;
+        stdout += i;
+      }
     }
+    return stdout;
   }
-  return 1;
-}
+};
 
-function calcPrime(stdin) {
-  var i = 1, count = 1, stdout = 0;
-  for (i; stdin >= count; i += 1) {
-    if (isPrime(i) === 1) {
-      count += 1;
-      stdout += i;
-    }
-  }
-  return stdout;
-}
-
-var stdout = calcPrime(10000);
+var stdout = Prime.calc(10000);
 console.log(stdout);
