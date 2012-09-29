12df77bb92f9b0d738c0e3abd6786f9ad0e31c7e
hidehigo <hidehigo@gmail.com>
2012-09-30 01:18:41 +0900

    update methane.py

--- scripts/yapc2012/methane.py
+++ scripts/yapc2012/methane.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python -S
-# coding: utf-8
-from math import log
-
 def main(N):
-    if N <= 3:
-        return sum([2,3,5][:N])
+    from math import log
+    if N <= 6:
+        return sum([2,3,5,7,11,13][:N])
 
-    # http://ja.wikipedia.org/wiki/素数定理
-    # N 個目の素数 < MAX_NUM が証明されてる
     MAX_NUM = int(N*log(N) + N*log(log(N)) + 1)
 
     L = bytearray(MAX_NUM)
-    F = b'1' * MAX_NUM
+    F = bytearray(b'1' * MAX_NUM)
     S = 2
     i = 2
     L[::2] = F[::2]
@@ -23,12 +18,6 @@ def main(N):
         L[::i] = F[::i]
 
     return S
-
-#import sys
-#if 'bench' in sys.argv:
-#    assert 496165411 == main(10000)
-#    import timeit
-#    print timeit.timeit(lambda: main(10000), number=100)
 
 print main(10000)
 
