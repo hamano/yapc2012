64b362fc524481b060316cf5e3c9bd87f8729932
kazuhiro homma <kazu.homma@gmail.com>
2012-09-28 22:11:12 -0400

    modified silvers

--- scripts/yapc2012/silvers.pl
+++ scripts/yapc2012/silvers.pl
@@ -1,27 +1,28 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use List::Util qw(sum);
 
-my $MAX_NUM = 104729;
+my $MAX_NUM = 104729; # 裏技
 my @table = (0..$MAX_NUM);
-my @primes = ();
+my $result = 2 + 3; # 最初だけ特別扱い
 
 sub is_prime {
     my $num = shift;
     return unless $table[$num];
-    push @primes, $num;
+    $result += $num;
 
     # 素数の倍数はもういらないので判定ついでに落とす
-    my $multi = $num + $num;
+    my $multi = $num * $num;
     while ( $multi <= $MAX_NUM ) {
         delete $table[$multi];
         $multi += $num;
     }
 }
 
-my $n = 1;
-while ( @primes < 10000 ) {
-    is_prime(++$n);
+# 5以上の素数は6n-1, 6n+1であらわすことができる
+my $n = 0;
+while ( $n < $MAX_NUM ) {
+    $n += 6;
+    is_prime($n + $_) for (-1, 1);
 }
-print sum(@primes);
+print $result;
