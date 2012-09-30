0f30a0061b450fa8745df645162c5812620e6f75
kazuhiro homma <kazu.homma@gmail.com>
2012-09-29 22:53:23 -0400

    modified silvers.js

--- scripts/yapc2012/silvers.js
+++ scripts/yapc2012/silvers.js
@@ -2,36 +2,35 @@ var primes = [2, 3, 5, 7, 11]; // 問題で与えられてるから使うよ
 
 var isPrime = function(n) {
   for (var i=0, l=primes.length; i < l; ++i) {
-    if (primes[i] * primes[i] > n) break;
-    if (n % primes[i] === 0) return;
+    if (n % primes[i] === 0) return false;
+    if (primes[i] * primes[i] > n) return true;
   }
-  return 1;
+  return true;
 };
 
 var sumPrimes = function(primeCount) {
-  var sum = 0;
-  var count = primes.length;
-  if (primeCount <= count) {
-    for(var i=0; i < primeCount; ++i) sum = sum + primes[i];
-  }
-  else {
-    var n = 0;
-    sum = 28; // 問題にあるから使うよ
+  // 今回の問題に関しては不要
+  // var sum = 0;
+  // if (primeCount <= primes.length) {
+  //   for(var i=0; i < primeCount; ++i) sum += primes[i];
+  // }
+  // else {
+    var i, t, n = 0, sum = 28; // sumは問題に載ってるから使うよ
+
     // 12以降の素数を12で割った余りは下のどれかになる
-    var rest = [1, 5, 7, 11];
-    var rcount = rest.length;
-    while ( 1 ) {
+    var rest = [1, 5, 7, 11], rcount = 4;
+    while ( true ) {
       n = n + 12;
-      for (var i=0; i < rcount; ++i) {
-        var testNum = n + rest[i];
-        if ( isPrime(testNum) ) {
-          count = primes.push(testNum);
-          sum = sum + testNum;
-          if (count >= primeCount) return sum;
+      for (i=0; i < rcount; ++i) {
+        t = n + rest[i];
+        if ( isPrime(t) ) {
+          sum += t;
+          primes[primes.length] = t;
+          if (primes.length >= primeCount) return sum;
         }
       }
     }
-  }
+  // }
   return sum;
 };
 
