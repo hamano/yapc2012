39bee1717e5e62290fca271f121f1fed2927628b
hidehigo <hidehigo@gmail.com>
2012-09-30 20:08:00 +0900

    HallOfFame kfly8.pl

---
+++ scripts/yapc2012_hall_of_fame/kfly8.pl
@@ -0,0 +1,12 @@
+use feature 'say';
+ 
+say chr oct oct ord qw/do/;
+say chr oct oct ord q mkdir m ;
+say chr oct oct ord qw/for/;
+say chr oct oct oct ord q eq le;
+say chr oct ord uc qw /bind/;
+say chr oct ord uc q each ne;
+say chr oct oct ord qw/dump/;
+say chr oct oct oct ord q eq ne;
+say chr oct oct oct ord q eq ne;
+
