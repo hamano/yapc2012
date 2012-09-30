45e0416aaeec37c9ad3c5b9f0902875ad0da597e
hidehigo <hidehigo@gmail.com>
2012-09-30 20:11:27 +0900

    update hirakun.pl

--- scripts/yapc2012/hirakun.pl
+++ scripts/yapc2012/hirakun.pl
@@ -4,31 +4,47 @@ use strict;
 my $total = 0;
 my @sosu = ();
 my $myNum;
-
-sub check_sosu {
-    my $num = shift;
-    for my $n (@sosu) {
-        last if $n * $n > $num;
-        return 0 if ($num % $n ==0);
-    }
-    return 1;
-}
+my $check;
 
 #多行エラトステネス法で数字を絞り込む
 #前準備
-for (2..30) {
-    if (check_sosu($_)) {
-        push @sosu,$_;
-        $total += $_;
+for my $i (2..30) {
+    $check = 1;
+    for my $n (@sosu) {
+      if ($n * $n > $i) {
+        $check = 1;
+        last;
+      } elsif($i % $n ==0) {
+        $check = 0;
+        last;
+      }
+    }
+    if ($check) {
+      push @sosu,$i;
     }
 }
-for (my $i = 30; @sosu < 10000; $i +=30) {
-    for my $j (1,7,11,13,17,19,23,29) {
-        $myNum = $i + $j;
-        if (check_sosu($myNum)) {
-            push @sosu,$myNum;
-            $total += $myNum;
-        }
+
+for (my $j = 30; @sosu < 10000; $j +=30) {
+  for my $k (1,7,11,13,17,19,23,29) {
+    $myNum = $j + $k;
+    $check = 1;
+    for my $n (@sosu) {
+      if ($n * $n > $myNum) {
+        $check = 1;
+        last;
+      } elsif($myNum % $n ==0) {
+        $check = 0;
+        last;
+      }
     }
+    if ($check) {
+      push @sosu,$myNum;
+    }
+  }
+}
+for (@sosu) {
+  $total += $_;
 }
 print $total . "\n";
+exit;
+
