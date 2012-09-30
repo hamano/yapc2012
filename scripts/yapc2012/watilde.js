9e8100683d1f071f3030b3b5ba38bee59d5d0301
hidehigo <hidehigo@gmail.com>
2012-10-01 02:07:31 +0900

    update watilde.js

--- scripts/yapc2012/watilde.js
+++ scripts/yapc2012/watilde.js
@@ -1,5 +1,5 @@
 (function (stdin) {
-  var i = 13, count = 6, stdout = 28,  primes = [2, 3, 5, 7, 11];
+  var i = 13, count = 6, stdout = 28, primes = [2, 3, 5, 7, 11], inc = [4, 2], inc_i = 1, mls = Math.log(stdin), maximum = Math.sqrt(stdin * (mls + Math.sqrt(mls)));
   function primeIs(n) {
     var j = 0, k = Math.sqrt(n);
     for (j; primes[j] <= k; j += 1) {
@@ -7,25 +7,13 @@
     }
     return true;
   }
-  if (6 <= stdin) {
-    (function () {
-      var increase = [4, 2], increase_i = 1, maximum = Math.sqrt(stdin * (Math.log(stdin) + Math.log(Math.log(stdin))));
-      for (i; count <= stdin; i += increase[increase_i]) {
-        if (primeIs(i)) {
-          if (i <= maximum) { primes[primes.length] = i; }
-          stdout += i;
-          count += 1;
-        }
-        if (increase_i === 0) {
-          increase_i += 1;
-        } else {
-          increase_i = 0;
-        }
-      }
-    }());
-  } else if (1 <= stdin && stdin <= 5) {
-    stdout = 0;
-    for (i = 0; i < stdin; i += 1) { stdout += primes[i]; }
+  for (i; count <= stdin; i += inc[inc_i]) {
+    if (primeIs(i)) {
+      if (i <= maximum) { primes[primes.length] = i; }
+      stdout += i;
+      count += 1;
+    }
+    (inc_i === 0) ? inc_i += 1 : inc_i = 0;
   }
   console.log(stdout);
 }(10000));
