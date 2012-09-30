217db66b123f955d8a4d8ec728cebeccbb3c279a
hidehigo <hidehigo@gmail.com>
2012-09-30 19:58:16 +0900

    update watilde.js

--- scripts/yapc2012/watilde.js
+++ scripts/yapc2012/watilde.js
@@ -1,19 +1,32 @@
-function primeIs (n) {
-  if (n % 2 === 0) { return 0; }
-  var i = 3;
-  for (i; i * i <= n; i += 2) {
-    if (n % i === 0) { return 0; }
-  }
-  return 1;
-}
-(function () {
-  var i = 2, count = 2, stdout = 2;
-  for (i; 10000 >= count; i += 1) {
-    if (primeIs(i) === 1) {
-      count += 1;
-      stdout += i;
+(function (stdin) {
+  var i = 13, count = 6, stdout = 28,  primes = [2, 3, 5, 7, 11];
+  function primeIs(n) {
+    var j = 0, k = Math.sqrt(n);
+    for (j; primes[j] <= k; j += 1) {
+      if (n % primes[j] === 0) { return false; }
     }
+    return true;
+  }
+  if (6 <= stdin) {
+    (function () {
+      var increase = [4, 2], increase_i = 1, maximum = Math.sqrt(stdin * (Math.log(stdin) + Math.log(Math.log(stdin))));
+      for (i; count <= stdin; i += increase[increase_i]) {
+        if (primeIs(i)) {
+          if (i <= maximum) { primes[primes.length] = i; }
+          stdout += i;
+          count += 1;
+        }
+        if (increase_i === 0) {
+          increase_i += 1;
+        } else {
+          increase_i = 0;
+        }
+      }
+    }());
+  } else if (1 <= stdin && stdin <= 5) {
+    stdout = 0;
+    for (i = 0; i < stdin; i += 1) { stdout += primes[i]; }
   }
   console.log(stdout);
-})();
+}(10000));
 
