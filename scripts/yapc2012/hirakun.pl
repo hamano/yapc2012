6765dca8c4dded77bd82c6b373614e6b0e4048c6
hidehigo <hidehigo@gmail.com>
2012-10-02 08:55:41 +0900

    update hirakun.pl

--- scripts/yapc2012/hirakun.pl
+++ scripts/yapc2012/hirakun.pl
@@ -1,49 +1,24 @@
 #!/usr/bin/perl
-use utf8;
 use strict;
-my $total = 0;
-my @sosu = ();
-my $myNum;
-my $check;
-
-#多行エラトステネス法で数字を絞り込む
-#前準備
-for my $i (2..30) {
-    $check = 1;
-    for my $n (@sosu) {
-      if ($n * $n > $i) {
-        $check = 1;
-        last;
-      } elsif($i % $n ==0) {
-        $check = 0;
-        last;
-      }
-    }
-    if ($check) {
-      push @sosu,$i;
-    }
-}
-
-for (my $j = 30; @sosu < 10000; $j +=30) {
-  for my $k (1,7,11,13,17,19,23,29) {
-    $myNum = $j + $k;
-    $check = 1;
-    for my $n (@sosu) {
-      if ($n * $n > $myNum) {
-        $check = 1;
-        last;
-      } elsif($myNum % $n ==0) {
-        $check = 0;
-        last;
-      }
-    }
-    if ($check) {
-      push @sosu,$myNum;
-    }
+use integer;
+my @primes = ();
+my $max;
+my $count = 1;
+my $total = 2;
+my $i = 1;
+my $j;
+#奇数のみを対象にする
+MAIN:
+while (1) {
+  $i += 2;
+  $max = sqrt($i);
+  for $j (@primes) {
+    next MAIN if ($i % $j == 0);
+    last if ($j > $max);
   }
-}
-for (@sosu) {
-  $total += $_;
+  push @primes,$i;
+  $total += $i;
+  last if (++$count == 10000);
 }
 print $total . "\n";
 
