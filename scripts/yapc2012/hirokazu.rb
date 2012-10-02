ca4198de1a6690b9d79fe9f615034d5318c0a141
hidehigo <hidehigo@gmail.com>
2012-10-02 09:05:45 +0900

    update hirokazu.rb

--- scripts/yapc2012/hirokazu.rb
+++ scripts/yapc2012/hirokazu.rb
@@ -1,19 +1,20 @@
 def isPrime(n)
-  i=3
-  while i*i<=n do
-    return false if n%i==0
-    i+=2
-  end
-  return true
+        $prime.each do |x|
+                break if n<x*x
+                return false if n%x==0
+        end
+        return true
 end
-sum,c=2,1
+$prime=[]
+sum,cnt=2,1
 i=3
-while c<10000 
-  if isPrime(i)
-    sum+=i
-    c+=1
-  end
-  i+=2
+while cnt<10000
+        if isPrime(i)
+                cnt+=1
+                sum+=i
+                $prime<<i
+        end
+        i+=2
 end
 puts sum
 
