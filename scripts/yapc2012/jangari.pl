a2c511c3cc147526705ffd601962eee18d22298b
hidehigo <hidehigo@gmail.com>
2012-10-03 09:46:33 +0900

    update jangari.pl

--- scripts/yapc2012/jangari.pl
+++ scripts/yapc2012/jangari.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
-my @list = (2);
-my ($max, $sum, $count) = (10000, 0, 0);
+my @list = (2, 3);
+my ($max, $sum, $count) = (9998, 5, 0);
 
-for(my $i = 2; $count < $max; $i++){
+for(my $i = 5, my $mod; $count < $max; ){
   foreach(@list){
     if($_ * $_ > $i){
       $count++;
@@ -15,6 +15,11 @@ for(my $i = 2; $count < $max; $i++){
       last;
     }
   }
+  $mod = $i % 6;
+  if($mod == 5){
+    $i += 2;
+  }elsif($mod == 1){
+    $i += 4;
+  }
 }
 print $sum;
-
