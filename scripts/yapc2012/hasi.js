fc18ab4d636176fd44e5ee2beb1b94a22b86209d
kazuhiro homma <kazu.homma@gmail.com>
2012-10-03 14:32:02 +0900

    modified hasi.js

--- scripts/yapc2012/hasi.js
+++ scripts/yapc2012/hasi.js
@@ -1,12 +1,28 @@
 var sumPrimes = function(n) {
-    var sup = Math.floor(n * (Math.log(n) + Math.log(Math.log(n))));
-    var sieveSize = Math.floor(sup/2);
+    var sieveSize = Math.floor(n * (Math.log(n) + Math.log(Math.log(n))) / 2 + 0.5);
+    var smallSieveSize = Math.floor(Math.sqrt(2 * sieveSize - 1) / 2 + 0.5);
+    var smallSieve = new Buffer(smallSieveSize);
+    smallSieve.fill(0);
+    for (var i = 1; 2*i*(i+1) < smallSieveSize; i++) if (!smallSieve[i]) {
+        var d = 2*i+1;
+        for (var j = 2*i*(i+1); j < smallSieveSize; j += d) smallSieve[j] = 1;
+    }
+    var prime = [];
+    for (var i = 1; i < smallSieveSize; i++) if (!smallSieve[i]) prime.push(i);
+    var mod = [1, 7, 11, 13, 17, 19, 23, 29];
+    var rem = new Buffer(15);
+    rem.fill(0);
+    for (var i = 0; i < mod.length; ++ i) rem[(mod[i]-1)/2] = 1;
     var sieve = new Buffer(sieveSize);
     sieve.fill(0);
-    var imax = Math.floor(Math.sqrt(sieveSize / 2 - 0.25) - 0.5);
-    for (var i = 1; i <= imax; i++) if (!sieve[i]) {
-        var d = 2*i+1;
-        for (var j = 2*i*(i+1); j < sieveSize; j += d) sieve[j] = 1;
+    for (var k = 2; k < prime.length; ++ k) {
+        var i = prime[k];
+        var s = 2*i*(i+1);
+        var d = (2*i+1)*15;
+        for (var l = 0; l < 15; ++ l) {
+            if (rem[s%15]) for (var j = s; j < sieveSize; j += d) sieve[j] = 1;
+            s += 2*i+1;
+        }
     }
     var sum = 2+3+5;
     var num = 3;
